2cc48c4ef7a1a53108bcdb761a2d3071ef4f8124
Swapnil Patil <102506228+Swapnil639@users.noreply.github.com>
2022-05-31 16:19:23 +0530

    Maximum and Minimum value

--- src/MaxAndMin.java
+++ src/MaxAndMin.java
@@ -8,10 +8,35 @@ public class MaxAndMin {
         a=scanner.nextInt();
         b=scanner.nextInt();
         c=scanner.nextInt();
-        System.out.println(a+b*c);
-        System.out.println(c+a/b);
-        System.out.println(a%b+c);
-        System.out.println(a*b+c);
+        
+        int w = a+b*c;
+        int x = c+a/b;
+        int y = a%b+c;
+        int z = a*b+c;
+        
+        System.out.println(w);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        
+        if(w>x && w>y && w>z)
+            System.out.println("W is Maximum :"+w);
+        else if(x>w && x>y && x>z)
+            System.out.println("X is Maximum :"+x);
+        else if(y>w && y>x && y>z)
+            System.out.println("Y is Maximum :"+y);
+        else
+            System.out.println("Z is Maximum :"+z);
+
+        if(w<x && w<y && w<z)
+            System.out.println("W is Minimum :"+w);
+        else if(x<w && x<y && x<z)
+            System.out.println("X is Minimum :"+x);
+        else if(y<w && y<x && y<z)
+            System.out.println("Y is Minimum :"+y);
+        else
+            System.out.println("Z is Minimum :"+z);
+
 
     }
 }
